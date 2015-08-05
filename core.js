734d0b58b1b9acce098a35f6e609fc9d609cc2df
jnkFC <jithesh@fusioncharts.com>
2015-08-05 17:30:07 +0530

    1st

---
+++ core.js
@@ -0,0 +1,25 @@
+function core()
+{
+  var lib={};
+    return Object.freeze({
+      showlib:lib,
+
+      register:function (key,val)
+        {
+          if(typeof lib[key]==='undefined') 
+          {
+            Object.defineProperty(lib,key,{value:val, enumerable: false,configurable: false,writable: false,});  //Explicitly making immutable object
+            
+          }
+          
+          else
+          {
+            console.log("key already exists");
+          }
+        },
+      getValue:function setter(key)
+        {
+          return lib[key];
+        }
+  });
+}
