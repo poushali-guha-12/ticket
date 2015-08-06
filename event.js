17e84cd7bff296ebc37d5e5d72e32f10c2d071c7
ayekpamkishan <ayekpam@fusioncharts.com>
2015-08-06 21:14:41 +0530

    edited

--- event.js
+++ event.js
@@ -1,59 +1,114 @@
-
 //FUNCTION TO ADD EVENT EVENT LISTENER
-function addEvent(id,events)
-{
-	var eventName,element;
+function event() {
+    var user = userdetail.getValue("user_details");
+    var draw = userdetail.getValue("draw");
+    var userjson = userdetail.getValue("forum_json");
+    var role, div1, div2, div3, div4;
 
-	element=document.getElementById(id);
-  for (eventName in events) {
-            element.addEventListener(eventName, events[eventName]);
-      }
-}//END OF addEvent
+    function addEvent(id, events) {
+        var eventName, element;
+        if (document.getElementById(id) !== null) {
+            element = document.getElementById(id);
+            for (eventName in events) {
+                element.addEventListener(eventName, events[eventName]);
+            }
+        }
+    } //END OF addEvent
+    //USER LOG IN BY ID
 
-addEvent("b1",{click:function(){alert("go clicked");}});
-addEvent("b2",{click:function(){alert("goo clicked");}});
-//USER LOG IN BY ID
-addEvent("go1",{click:function(){
-                                  getPriviledgeById();
-                                }
-              }
-        );
-//ADD USER 
-addEvent("add",{click:function(){
-                                  setUser(document.getElementById(name).value,document.getElementById(id).value,document.getElementById(role).value);
-                                }
+    div1 = draw.login();
+    addEvent(div1.go1, {
+        click: function(){
+                            getPriviledgeById();
+                        }
+                    }
+            );
+    div2 = draw.addUser();
+    document.getElementById('addUserDiv').style.display="none";
+    addEvent(div2.add_user, {
+                        click: function() {
+
+                            if (document.getElementById(div2.login_id).value === '' || document.getElementById(div2.name).value === '' || document.getElementById(div2.role).value === '') {
+                                
+                                alert("provide all detailes");
+                            } 
+                            else 
+                            {
+                                var sucmsg = userjson.setUser(document.getElementById(div2.login_id).value, document.getElementById(div2.name).value, document.getElementById(div2.role).value);
+                                alert(document.getElementById(div2.login_id).value);
+                                alert(document.getElementById(div2.name).value);
+                                alert(document.getElementById(div2.role).value);
+                                alert(sucmsg);
+                            }
+
+
+                        }
+                    });
+    
+
+    //GET PRIVILEDGE BY ID AND DRAW TEMPLATE ACCORDINGLY
+    function getPriviledgeById() {
+                var pri = [];
+
+                //isPriviledge will return the priviledges of the passed ID
+                pri = user.isPrivilege(document.getElementById(div1.login_id).value);
+
+                for (var i = 0; i < pri.length; i++) {
+                    //drawTemplate will create the templates
+
+                    if (pri[i] === "add") {
+                    if(document.getElementById('insertionDiv')==null){
+                        div4 = draw.createTicket();
+                        addEvent("go2", {
+                            click: function() {
+
+                                ticketId = Math.floor((Math.random() * 999999) + 100000);
+                                //var user=userdetail.getValue("user_details");
+
+                                var name = user.getName(document.getElementById(div1.login_id).value);
+
+                                console.log(name);
+                                alert(div4.insert_header);
+                                alert(div4.insert_description);
+
+                                draw.showTicket(ticketId, name, document.getElementById(div4.insert_header).value, document.getElementById(div4.insert_description).value);
+                                document.getElementById(div4.insert_header).value = '';
+                                document.getElementById(div4.insert_description).value = '';
+                            }
+                        });
+                    }
+                    }
+
+                    if (pri[i] === "insertUser") {
+                        document.getElementById(div2.add_user).disabled = false;
+                        document.getElementById("viewDiv").style.display = "none";
+                        if(document.getElementById("insertionDiv")!=null){
+                            document.getElementById("insertionDiv").style.display = "none";
+                        }
+                        if (document.getElementById('ticketDiv0') !== null) {
+                            document.getElementById('ticketDiv0').remove();
+                        }
+                        if (document.getElementById('div4') !== null) {
+                            document.getElementById('div4').remove();
+                        }
+
+                    } 
+                    else {
+                        document.getElementById(div2.add_user).disabled = true;
+
+                        document.getElementById("viewDiv").style.display = "block";
+                        document.getElementById("insertionDiv").style.display = "block";
+                    }
+                    if (pri[i] === "Id not Matching!!!") {
+                        alert("Id not match");
+                    } else {
+
+                        document.getElementById('addUserDiv').style.display = "block";
+                    }
+
+                    //drawTemplate(pri[i]);
                 }
-        );
-
-//EXPORT ALL TICKET 
-addEvent("exportAll",{click:function(){
-                                  exportAllTicket();
-                                }
-              }
-        );
-//ADD TICKET 
-addEvent("go2",{click:function(){
-                                  ticketId = Math.floor((Math.random() * 999999) + 100000);
-                                  name = getName(document.getElementById('userId').value);
-                                  //addTicket will return the specific EXPORT button ID
-                                  exportId = addTicket(ticketId,name,header,description);
-                                  //EXPORT SINGLE TICKET 
-                                  addEvent("exportId",{click:function(){
-                                                                    exportTicket(this);
-                                                                  }
-                                                }
-                                          );
-                                }
-        );
-//GET PRIVILEDGE BY ID AND DRAW TEMPLATE ACCORDINGLY
-function getPriviledgeById()
-{
-  var pri=[];
-  //isPriviledge will return the priviledges of the passed ID
-  pri=isPriviledge(document.getElementById('userId').value);
-  for(var i=0;i<pri.length;i++)
-  {
-    //drawTemplate will create the templates
-    drawTemplate(pri[i]);
-  }
+                return user.getRole(document.getElementById(div1.login_id).value);
+            }
 }
+userdetail.register("event", event());
