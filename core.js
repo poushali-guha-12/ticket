030e18798f618d1bdf42e0153bdfdb06b92d52ac
Sayan Sikdar <sayan@fusioncharts.com>
2015-08-07 11:47:38 +0530

    id added for authetication for dynamic change of the library fucntion

--- core.js
+++ core.js
@@ -1,26 +1,45 @@
 var lib={};
 function core()
 {
- 
-    return Object.freeze({
-      showlib:lib,
+	Object.defineProperty(core,"getAuthetication",{
+		value:function(val){var a=Math.random()*100000000;var b=Math.random()*100;var c=Math.round(a/b); return c;},
+        configurable:false,
+		writable:false,
+		enumerable:false,});
 
-      register:function (key,val)
-        {
-          if(typeof lib[key]==='undefined') 
-          {
-            Object.defineProperty(lib,key,{value:val, enumerable: false,configurable: false,writable: false,});  //Explicitly making immutable object
-            
-          }
-          
-          else
-          {
-            console.log("key already exists");
-          }
-        },
-      getValue:function setter(key)
-        {
-          return lib[key];
-        }
-  });
+
+
+	Object.defineProperty(core,"register",{
+		value:function(val,lib_key,pass)
+			{
+				if(typeof lib[lib_key]==='undefined')
+			  		lib[lib_key]=[val,pass];
+			  	else if(typeof lib[lib_key]==="object" && lib[lib_key][1]==pass)
+			  	{
+			  		lib[lib_key]=[val,pass];
+			  		console.log("Congratulation,you have set a new value to the core library!!");
+			  	}
+			  	else if(typeof lib[lib_key]==="object" && lib[lib_key][1]!==pass)
+			  	{
+			  		console.log("You are not authorized to do this operation");
+			  	}
+
+		    },
+		configurable:false,
+		writable:false,
+		enumerable:false,});
+
+
+	Object.defineProperty(core,"Access_lib",{
+		value:function(lib_key)
+			  	{
+                 	return lib[lib_key];
+				},
+		configurable:false,
+		writable:false,
+		enumerable:false
+	});
+
 }
+core();
+
